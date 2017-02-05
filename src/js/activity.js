d5c502738ed9839f0e0438eee086ae6935073669
JasonLinn <cheng11220@gmail.com>
2017-02-05 16:37:45 +0800

    activity_0205

---
+++ src/js/activity.js
@@ -0,0 +1,21 @@
+$(function(){
+	$('#a-phone-mapBtn').click(function(){
+
+	})	
+	$('#a-phone-input_1').focus(function(){
+		$('#a-phone-input_1').hide();
+		$('.fa-chevron-up').show(500).css({
+			'display':'block'
+		});
+
+		$('.a-phone-input').show();
+		$('.fa-search').hide();
+
+	})
+	$('.fa-chevron-up').click(function(){
+		$('.a-phone-input').hide();
+		$('.fa-chevron-up').hide();
+		$('#a-phone-input_1').show();
+		$('.fa-search').show();
+	})
+})
