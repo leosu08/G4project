15d2717868422f4334d8c68177a468ec94745d4d
jun <jiung81625@gmail.com>
2017-02-07 15:09:46 +0800

    update

--- src/js/magnific.js
+++ src/js/magnific.js
@@ -21,7 +21,17 @@ $('#a-popups').magnificPopup({
   },
   midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 });
-
+// 相片上傳
+$('#inline-upload').magnificPopup({
+  delegate: 'a',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
 //test
 
 
