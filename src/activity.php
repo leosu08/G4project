02172884eeb4917472feb6d3eba6fcbeb1559901
JasonLinn <cheng11220@gmail.com>
2017-02-25 22:29:49 +0800

    no message

--- src/activity.php
+++ src/activity.php
@@ -1,7 +1,8 @@
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -558,8 +559,8 @@
 <section id="a_1activity_wrapper" >
    <div id="close-btn">
 		<div id="closeIt">CLOSE</div>
-		
-		<img class="closeIco" src="img/a_005.jpg" alt="close icon">
+		 <svg class="closeIco" xmlns="img/close.svg" version="1.1"></svg>
+		<!-- <img class="closeIco" src="img/a_005.jpg" alt="close icon"> -->
 	</div>
 	<div class="container">
 	<div class="aa_content">
--- src/js/activity.js
+++ src/js/activity.js
@@ -2,7 +2,6 @@ $(function(){
 document.getElementById('act_img').onchange = fileChange;
   $('#multiple').hide();
 
-
   $('#a-add-mutiple1').focus(function(){
     $('#multiple').show().css({
       'position':'fixed',
@@ -33,63 +32,18 @@ document.getElementById('act_img').onchange = fileChange;
     actCla_no=1;
     console.log(actCla_no);
   })
-	// $('#a-phone-mapBtn').click(function(){
-	// 	$('.a-map').css({
-	// 		'display':'block'
-	// 	});
-	// 	// initAutocomplete();
-	// })	
-	$('#a-phone-input_1').focus(function(){
-		$('#a-phone-input_1').hide();
-		$('.fa-chevron-up').slideDown(500).css({
-			'display':'block'
-		});
-
-		$('.a-phone-input').slideDown();
-		$('.fa-search').hide();
-
-	})
-	$('.fa-chevron-up').click(function(){
-		$('.a-phone-input').hide();
-		$('.fa-chevron-up').hide();
-		$('#a-phone-input_1').slideDown();
-		$('.fa-search').slideDown();
-	})
-	$('#turnOff').click(function(){
-		console.log("click");
-		$.fn.fullpage.setAllowScrolling(false);
-  		
-	})
-
-	
-	// $('.a-add-put-p1').css('opacity','0');
-	// $('.a-add-put-p2').css('opacity','0');
-	// $('.a-add-put-p3').css('opacity','0');
-	// $('.a-add-put-p4').css('opacity','0');
-
-	
-$('#a-add-q1').mouseover(function(){
-		$('.a-add-put-p1').stop(true,false).fadeTo(1000,'1');
-		$('.a-add-p').not(".a-add-put-p1").fadeTo(1,'0');
-	})
 $('#a-add-q2').mouseover(function(){
     $('.a-add-put-p2').stop(true,false).fadeTo(1000,'1');
     $('.a-add-p').not(".a-add-put-p2").fadeTo(1,'0');
     actCla_no=2;
     console.log(actCla_no);
   })
-		$('.a-add-put-p2').stop(true,false).fadeTo(1000,'1');
-		$('.a-add-p').not(".a-add-put-p2").fadeTo(1,'0');
-	})
 $('#a-add-q3').mouseover(function(){
     $('.a-add-put-p3').stop(true,false).fadeTo(1000,'1');
     $('.a-add-p').not(".a-add-put-p3").fadeTo(1,'0');
     actCla_no=3;
     console.log(actCla_no);
   })
-		$('.a-add-put-p3').stop(true,false).fadeTo(1000,'1');
-		$('.a-add-p').not(".a-add-put-p3").fadeTo(1,'0');
-	})
 $('#a-add-q4').mouseover(function(){
     $('.a-add-put-p4').stop(true,false).fadeTo(1000,'1');
     $('.a-add-p').not(".a-add-put-p4").fadeTo(1,'0');
@@ -99,36 +53,43 @@ $('#a-add-q4').mouseover(function(){
 /*=====================================
             act_class mouseover
     =======================================*/
-		$('.a-add-put-p4').stop(true,false).fadeTo(1000,'1');
-		$('.a-add-p').not(".a-add-put-p4").fadeTo(1,'0');
-	})
-
 $('#a-slide-1 ul li:first-child').mouseover(function(){
 		// console.log('over');
 		$('#a-ast-ini2').stop(true,false).fadeTo(1,'1');
 		$('#a-ast-ini1').fadeTo(1,'0');
 	})
+
+
+
 $('#a-slide-1 ul li:first-child').mouseleave('test',function(){
 		// console.log('over');
 		$('#a-ast-ini1').stop(true,false).fadeTo(1,'1');
 		$('#a-ast-ini2').fadeTo(1,'0');
 	})
+
+
 $('#a-slide-1 ul li:first-child').click(function(){
 		// console.log('over');
 		$('#a-ast-ini2').stop(true,false).fadeTo(1,'1');
 		$('#a-ast-ini1').fadeTo(1,'0');
 		$(this).unbind('mouseleave');
 	})
+
+
 $('#a-slide-1 ul li:nth-child(2)').mouseover(function(){
 		// console.log('over');
 		$('#a-ast-pro2').stop(true,false).fadeTo(1,'1');
 		$('#a-ast-pro1').fadeTo(1,'0');
 	})
+
+
 $('#a-slide-1 ul li:nth-child(2)').mouseleave('test',function(){
 		// console.log('over');
 		$('#a-ast-pro1').stop(true,false).fadeTo(1,'1');
 		$('#a-ast-pro2').fadeTo(1,'0');
 	})
+
+
 $('#a-slide-1 ul li:nth-child(2)').click(function(){
 		// console.log('over');
 		$('#a-ast-pro2').stop(true,false).fadeTo(1,'1');
@@ -136,7 +97,6 @@ $('#a-slide-1 ul li:nth-child(2)').click(function(){
 		$(this).unbind('mouseleave');
 	})
 
-<<<<<<< HEAD
 
 /*=====================================
             手機板的input縮放
@@ -615,8 +575,4 @@ $('#a-se').click(function(){
 
 	
 
-=======
-				
->>>>>>> 37054db9ce31a52e4a8ada23b8773f7f84b807eb
 	
-})//window.function
